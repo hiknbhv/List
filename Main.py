18d5f0ee0087053642e3c8cb5f6cd71d935facca
Tamilea pimpton <pimptam0000@warrren.k12.in.us>
2024-11-07 10:06:45 -0500

    hg

---
+++ Main.py
@@ -0,0 +1,20 @@
+color_list = ["red", "green", "blue", "yellow"]
+print(color_list[0])
+print(color_list[-1])
+
+num_list = [4, 6, 4, 2, 9, 4, 1]
+
+num_list.append(5)
+print(num_list)
+
+num_list.remove(4)
+print(num_list)
+
+print(num_list.count(3))
+
+print(num_list.index(9))
+
+fruit_list = ["apple", "banana", "cherry"]
+fruit_list[1] = "orange"
+fruit_list.pop()
+print(fruit_list)
